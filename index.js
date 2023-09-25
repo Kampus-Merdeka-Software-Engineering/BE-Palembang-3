cc64c1d60f20fd50586cbd8eec92e6016316a3f0
Nur Sya'bani Ahwan <nursyabaniahwan@gmail.com>
2023-09-25 14:43:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ import productsRouter from "./src/routes/products.js";
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    next();
+});
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.raw());
 app.use(bodyParser.json());
